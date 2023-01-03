5d18bab68b6ad2ad05de638eac47c8dca49ba61e
Huda Prasetyo <real.hudaprasetyo@gmail.com>
2023-01-04 01:53:43 +0700

    Fix: remove whitespace

--- web/src/features/project-transaction/ui/ProjectTransactionIndex/components/Modal/index.tsx
+++ web/src/features/project-transaction/ui/ProjectTransactionIndex/components/Modal/index.tsx
@@ -40,7 +40,6 @@ import { CloudDownloadOutlined } from '@ant-design/icons'
 // PDF Make
 import * as pdfMake from 'pdfmake/build/pdfmake'
 import * as pdfFonts from 'pdfmake/build/vfs_fonts'
-
 ;(pdfMake as any).vfs = pdfFonts.pdfMake.vfs
 
 const Modal = memo(
